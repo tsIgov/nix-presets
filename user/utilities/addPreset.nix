af029311f9d4d078b03762adaacba918ea0ad04c
Tsvetan Igov <ts.igov@outlook.com>
2025-02-06 02:23:13 +0200

    Merged the system presets into aether

--- user/utilities/addPreset.nix
+++
@@ -1,9 +0,0 @@
- path: pkgs: 
-{ aether, ... }@args: 
-{ 
-	imports = 
-		if pkgs == null then 
-			aether.lib.moduleUtils.listModulesRecursively path
-		else 
-			[ (aether.lib.moduleUtils.createRecursiveModuleWithOverrides path { inherit pkgs;}) ];
-}
